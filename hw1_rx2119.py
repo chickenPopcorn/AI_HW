4ac104ac9fbfe44ae69dc8fd35137f9cf4a18a66
Ruicong Xie <rxie25@gmail.com>
2016-06-01 03:41:11 -0400

    modified class structure and test

--- hw1_rx2119.py
+++ hw1_rx2119.py
@@ -5,17 +5,17 @@ class state:
         self.n = n
         self.full_size = n**2
         self.list = range(0,self.full_size)
-
+        self.move = ""
         self.parent = parent
         self.child = child
-        
+
     def printState(self):
         for i in range(0, self.n):
             row = []
             for l in range(0, self.n):
                 row.append(self.list[l+i*self.n])
             print row
-            
+
     def returnState(self):
         result = []
         for i in range(0, self.n):
@@ -25,27 +25,34 @@ class state:
             result.append(row)
         return result
 
-    def move(self, direct):
+    def moveDirect(self, direct):
         blank_pos = self.list.index(0)
         if direct == "LEFT" and blank_pos % self.n != self.n-1:
-            self.parent = copy.deepcopy(self)
-            self.parent.child = self
-            self.list[blank_pos], self.list[blank_pos+1] = \
-            self.list[blank_pos+1], self.list[blank_pos]
-            
+            self.child = copy.deepcopy(self)
+            self.child.parent = self
+            self.child.list[blank_pos], self.child.list[blank_pos+1] = \
+            self.child.list[blank_pos+1], self.child.list[blank_pos]
         elif direct == "RIGHT" and blank_pos % self.n != 0:
-            self.parent = copy.deepcopy(self)
-            self.parent.child = self
-            self.list[blank_pos-1], self.list[blank_pos] = \
-            self.list[blank_pos], self.list[blank_pos-1]
+            self.child = copy.deepcopy(self)
+            self.child.parent = self
+            self.child.list[blank_pos-1], self.child.list[blank_pos] = \
+            self.child.list[blank_pos], self.child.list[blank_pos-1]
         elif direct == "DOWN" and blank_pos <= (self.n-1)*self.n-1:
-            self.parent = copy.deepcopy(self)
-            self.parent.child = self
-            self.list[blank_pos+self.n], self.list[blank_pos] = \
-            self.list[blank_pos], self.list[blank_pos+self.n]  
+            self.child = copy.deepcopy(self)
+            self.child.parent = self
+            self.child.list[blank_pos+self.n], self.child.list[blank_pos] = \
+            self.child.list[blank_pos], self.child.list[blank_pos+self.n]
         elif direct == "UP" and blank_pos >= self.n:
-            self.parent = copy.deepcopy(self)
-            self.parent.child = self
-            self.list[blank_pos-self.n], self.list[blank_pos] = \
-            self.list[blank_pos], self.list[blank_pos-self.n]
-    
+            self.child = copy.deepcopy(self)
+            self.child.parent = self
+            self.child.list[blank_pos-self.n], self.child.list[blank_pos] = \
+            self.child.list[blank_pos], self.child.list[blank_pos-self.n]
+        return self.child
+
+if __name__ == "__main__":
+    a = state(3)
+    print a
+    a.printState()
+    current = a
+    current = current.moveDirect("LEFT")
+    current.printState()
--- hw1_unittest.py
+++ hw1_unittest.py
@@ -2,45 +2,45 @@ import unittest
 from hw1_rx2119 import state
 
 class MyTest(unittest.TestCase):
-	def test_constructor(self):
-		a = state(3)
-		self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-		
-	def test_move(self):
-		a = state(3)
-		a.move("LEFT")
-		self.assertEqual(a.returnState(), [[1, 0, 2], [3, 4, 5], [6, 7, 8]])
-		a.move("LEFT")
-		self.assertEqual(a.returnState(), [[1, 2, 0], [3, 4, 5], [6, 7, 8]])	
-		a.move("LEFT")
-		self.assertEqual(a.returnState(), [[1, 2, 0], [3, 4, 5], [6, 7, 8]])	
-		
-		a.move("RIGHT")
-		self.assertEqual(a.returnState(), [[1, 0, 2], [3, 4, 5], [6, 7, 8]])	
-		a.move("RIGHT")
-		self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])	
-		a.move("RIGHT")
-		self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])	
-		
-		a.move("DOWN")
-		self.assertEqual(a.returnState(), [[3, 1, 2], [0, 4, 5], [6, 7, 8]])
-		a.move("DOWN")
-		self.assertEqual(a.returnState(), [[3, 1, 2], [6, 4, 5], [0, 7, 8]])
-		a.move("DOWN")
-		self.assertEqual(a.returnState(), [[3, 1, 2], [6, 4, 5], [0, 7, 8]])
-		
-		a.move("UP")
-		self.assertEqual(a.returnState(), [[3, 1, 2], [0, 4, 5], [6, 7, 8]])
-		a.move("UP")
-		self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-		a.move("UP")
-		self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-		
-		a.parent.parent.child.child.returnState()
-		self.assertEqual(a.parent.parent.child.child, a)
-		self.assertEqual(a.parent.parent.child.child.returnState(), a.returnState())
-		
-		
-		
+    def test_constructor(self):
+        a = state(3)
+        self.assertEqual(a.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+
+    def test_moveDirect(self):
+        a = state(3)
+        current = a
+        current = current.moveDirect("LEFT")
+        self.assertEqual(current.returnState(), [[1, 0, 2], [3, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("LEFT")
+        left = current
+        self.assertEqual(current.returnState(), [[1, 2, 0], [3, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("LEFT")
+        self.assertEqual(current, None)
+        # moving to right from left most
+        current = left.moveDirect("RIGHT")
+        self.assertEqual(current.returnState(), [[1, 0, 2], [3, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("RIGHT")
+        right = current
+        self.assertEqual(current.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("RIGHT")
+        self.assertEqual(current, None)
+        # moving down
+        current = right.moveDirect("DOWN")
+        self.assertEqual(current.returnState(), [[3, 1, 2], [0, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("DOWN")
+        down = current
+        self.assertEqual(current.returnState(), [[3, 1, 2], [6, 4, 5], [0, 7, 8]])
+        current = current.moveDirect("DOWN")
+        self.assertEqual(current, None)
+        # moving up
+        current = down.moveDirect("UP")
+        self.assertEqual(current.returnState(), [[3, 1, 2], [0, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("UP")
+        self.assertEqual(current.returnState(), [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+        current = current.moveDirect("UP")
+        self.assertEqual(current, None)
+        self.assertEqual(down.parent.parent.child.child, down)
+        self.assertEqual(left.parent.parent.child.child.returnState(), left.returnState())
+
 if __name__ == "__main__":
-	unittest.main(exit = False)
+    unittest.main(exit = False)
