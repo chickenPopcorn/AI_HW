6725cd59c72a0f670c087123f893e2077650e027
Ruicong Xie <ruicongxie@Ruicongs-MacBook-Pro.local>
2016-06-02 21:12:25 -0400

    bfs and dfs working

--- hw1_rx2119.py
+++ hw1_rx2119.py
@@ -1,24 +1,32 @@
 import copy
 import random
-import Queue
 import time
 import math
 import signal
 import sys
+from collections import deque
+from sets import Set
 
 def Exit_gracefully(signal, frame):
     print
     sys.exit(0)
 
 DIRECTION = ["UP", "RIGHT", "DOWN", "LEFT"]
-DIRECT_DICT = {"UP":"DOWN", "RIGHT":"LEFT", "DOWN":"UP", "LEFT":"RIGHT"}
+
 class State:
-    def __init__(self, n):
+    def __init__(self, n, original=None, parent=None, move=None):
         self.n = n
         self.full_size = n**2
-        self.list = range(0,self.full_size)
-        self.move = ""
-        self.parent = None
+        if original == None:
+            self.list = range(0,self.full_size)
+        else:
+            assert self.full_size == len(original)
+            self.list = list(original)
+        if move != None:
+            self.move = ''.join(move)
+        else:
+            self.move = None
+        self.parent = parent
         self.child = []
 
     def __str__(self):
@@ -29,10 +37,6 @@ class State:
                 row.append(self.list[l+i*self.n])
             board+=str(row)+"\n"
         return board
-
-    def takeList(self, newList):
-        assert len(newList) == self.full_size
-        self.list = newList
 
     def __eq__(self, other):
         return self.list == other.list
@@ -46,30 +50,20 @@ class State:
             result.append(row)
         return result
 
-    def legalMove(self, direct, blank_pos):
-        if (direct == "RIGHT" and blank_pos %self.n !=self.n-1) or \
-           (direct == "LEFT" and blank_pos %self.n != 0) or \
-           (direct == "DOWN" and blank_pos <= (self.n-1)*self.n-1) or \
-            direct == "UP" and blank_pos >= self.n:
+    def legalMove(self, direct, pos):
+        if (direct == "RIGHT" and pos %self.n !=self.n-1) or \
+           (direct == "LEFT" and pos %self.n != 0) or \
+           (direct == "DOWN" and pos <= (self.n-1)*self.n-1) or \
+            direct == "UP" and pos >= self.n:
                 return direct
         else:
             return "illegal move"
 
-    def cloneSelf(self):
-
-        a = State(self.n)
-        a.takeList(self.list)
-        a.parent = self
-        a.move = self.move
-        #self.child.append(copy.deepcopy(self))
-        self.child.append(a)
-
-
-    def moveDirect(self, direct):
+    def simulateMove(self, direct):
         blank_pos = self.list.index(0)
         move = self.legalMove(direct, blank_pos)
         if move != "illegal move":
-            self.cloneSelf()
+            self.child.append(State(self.n, self.list, self))
             self.child[-1].move = move
             if move == "RIGHT":
                 self.child[-1].list[blank_pos], self.child[-1].list[blank_pos+1] = \
@@ -87,6 +81,22 @@ class State:
         else:
             return None
 
+    def makeMove(self, direct):
+        blank_pos = self.list.index(0)
+        move = self.legalMove(direct, blank_pos)
+        if move != "illegal move":
+            if move == "RIGHT":
+                self.list[blank_pos], self.list[blank_pos+1] = \
+                self.list[blank_pos+1], self.list[blank_pos]
+            elif move == "LEFT":
+                self.list[blank_pos-1], self.list[blank_pos] = \
+                self.list[blank_pos], self.list[blank_pos-1]
+            elif direct == "DOWN":
+                self.list[blank_pos+self.n], self.list[blank_pos] = \
+                self.list[blank_pos], self.list[blank_pos+self.n]
+            else:
+                self.list[blank_pos-self.n], self.list[blank_pos] = \
+                self.list[blank_pos], self.list[blank_pos-self.n]
 
     def isGoalState(self):
         if self.list == range(0, self.full_size):
@@ -95,12 +105,12 @@ class State:
             return False
 
     def shuffle(self):
-        lastMove = None
-        for i in range (100):
-            random.choice(DIRECTION)
+        moves = random.random()*32
+        for i in range (int(moves)):
+            self.makeMove(random.choice(DIRECTION))
 
     def dfs(self):
-        visitedStates= []
+        visitedStates= Set()
         stack = []
         maxSize = 1
         current = self
@@ -109,20 +119,12 @@ class State:
         while not current.isGoalState() and len(stack) != 0:
             current = stack.pop()
             nodeExpand += 1
-            visitedStates.append(current.list)
-            '''
-            if current != None:
-                current.printState()
-                print "moved "+ current.move
-                print
-            else:
-                print "this one is None"
-                print
-            '''
+            visitedStates.add(tuple(current.list))
             for direct in DIRECTION:
-                childNode = current.moveDirect(direct)
-                if childNode != None and childNode.list not in visitedStates:
-                   stack.append(childNode)
+                childNode = current.simulateMove(direct)
+                if childNode != None:
+                    if not tuple(childNode.list) in visitedStates:
+                        stack.append(childNode)
             maxSize = max(len(stack),maxSize)
 
         if current is not None:
@@ -133,32 +135,21 @@ class State:
     def bfs(self):
         visitedStates = []
         maxSize = 1
-        que = Queue.Queue()
+        que = deque()
         nodeExpand = 0
         current = self
-        que.put(self)
-        while not current.isGoalState() and not que.empty():
-            current = que.get()
+        que.append(self)
+        while not current.isGoalState() and len(que) != 0:
+            current = que.popleft()
             nodeExpand += 1
-            visitedStates.append(current.list)
-            '''
-            if current != None:
-                current.printState()
-                print "moved "+ current.move
-                print
-            else:
-                print "this one is None"
-                print
-            '''
+            if current.list not in visitedStates:
+                visitedStates.append(copy.deepcopy(current.list))
             for direct in DIRECTION:
-                childNode = current.moveDirect(direct)
-                if childNode !=  None and childNode.list not in visitedStates:
-                    print 123
-                    que.put(childNode)
-            print current.list
-            print que.empty()
-            print current.isGoalState()
-            maxSize = max(que.qsize(),maxSize)
+                childNode = current.simulateMove(direct)
+                if childNode !=  None:
+                    if not childNode.list in visitedStates:
+                        que.append(childNode)
+            maxSize = max(len(que),maxSize)
         if current is not None:
             return current, maxSize, nodeExpand
         else:
@@ -188,45 +179,45 @@ class State:
             print "Cost of path = " + str(len(path))
             print "nodes expanded = "+ str(nodeExpand)
             print "max stack/queue size = "+str(size)
-            print "path to solution:" + str(path)
+            # print "path to solution:" + str(path)
         else:
             print "Solution not found"
-
-
-
-
+        return path
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, Exit_gracefully)
-    a = State(2)
-    print "initializing for BFS"
-    a.takeList([2,0,3,1])
-    print a
-    a.solving('bfs')
-    del a
+    testCases = [
+        (2, [2,0,3,1]),
+        (3, [1,2,5,3,0,4,6,7,8]),
+    ]
 
-    print
-    a = State(2)
-    print "initializing fro DFS"
-    a.takeList([2,0,3,1])
-    print a
-    a.solving('dfs')
-    del a
+    methods = [
+        "bfs", "dfs"
+    ]
+    '''
+    for case in testCases:
+        for method in methods:
+            a = State(case[0], case[1])
+            print "initializing for BFS"
+            print a
+            path = a.solving(method)
+            for i in path:
+                a.makeMove(i)
+            print "the solution is "+ str(a.isGoalState()) +"\n"
+            del a
+    '''
+    n = 0
+    for i in range(5):
+        n += 1
+        print "test case "+str(n)
+        for method in methods:
+            a = State(3)
+            a.shuffle()
+            print "initializing for "+method.upper()
+            print a
+            path = a.solving(method)
+            for i in path:
+                a.makeMove(i)
+            print "the solution is "+ str(a.isGoalState()) +"\n"
+            del a
 
-
-
-    print
-    a = State(3)
-    print "initializing fro BFS"
-    a.takeList([1,2,5,3,4,0,6,7,8])
-    print a
-    a.solving('bfs')
-    del a
-
-    print
-    a = State(3)
-    print "initializing fro DFS"
-    a.takeList([1,2,5,3,0,4,6,7,8])
-    print a
-    a.solving('dfs')
-    del a
